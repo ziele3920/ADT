2349a8536055244fcbd3cbea6878368b736d860c
ziele3920 <a.ziolkowski3920@wp.pl>
2019-09-01 10:43:38 +0200

    selection sorf for IList

--- src/main/java/pl/sda/List/MainList.java
+++ src/main/java/pl/sda/List/MainList.java
@@ -3,11 +3,12 @@ package pl.sda.List;
 public class MainList {
 
     public static void main(String[] args) {
-        IList list = new LinkedList();
+        SelectionSort ss = new SelectionSort();
+        IList list = new LinkedList<Integer>();
+        list.add(5);
         list.add(1);
         list.add(2);
-        list.add(3);
-
+        list = ss.sort(list);
         Object[] tab = list.getHolderView();
 
         for (Object val:tab)
@@ -15,7 +16,6 @@ public class MainList {
         System.out.println();
 
         int[] tabb =  {3, 1, 5, 11, 9};
-        SelectionSort ss = new SelectionSort();
         int[] sorted = ss.sort(tabb);
         for(int v:sorted)
             System.out.print(v + ", ");
--- src/main/java/pl/sda/List/SelectionSort.java
+++ src/main/java/pl/sda/List/SelectionSort.java
@@ -1,6 +1,18 @@
 package pl.sda.List;
 
 public class SelectionSort {
+
+    public IList sort(IList<Integer> listToSort) {
+        int firstUnsortedIndex = 0;
+        do {
+            int minIndex = findMinValueIndex(listToSort, firstUnsortedIndex);
+            int tmp = listToSort.get(minIndex);
+            listToSort.set(minIndex, listToSort.get(firstUnsortedIndex));
+            listToSort.set(firstUnsortedIndex, tmp);
+        } while(++firstUnsortedIndex != listToSort.size());
+        return listToSort;
+    }
+
     public int[] sort(int[] tableToSort) {
         int firstUnsortedIndex = 0;
         do {
@@ -10,6 +22,18 @@ public class SelectionSort {
             tableToSort[firstUnsortedIndex] = tmp;
         } while(++firstUnsortedIndex != tableToSort.length);
         return tableToSort;
+    }
+
+    private int findMinValueIndex(IList<Integer> list, int startIndex) {
+        int minValue = list.get(startIndex);
+        int minValueIndex = startIndex;
+        for(int i = startIndex+1; i < list.size(); i++) {
+            if(list.get(i) < minValue) {
+                minValue = list.get(i);
+                minValueIndex = i;
+            }
+        }
+        return minValueIndex;
     }
 
     private int findMinValueIndex(int[] table, int startIndex) {
